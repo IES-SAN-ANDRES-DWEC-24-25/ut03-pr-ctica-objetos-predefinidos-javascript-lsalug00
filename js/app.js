6acd6fc8dc310266402dd3fb43be785095b5ecac
lsalug00 <lidiasaludes@gmail.com>
2024-10-14 16:27:55 +0200

    ej1 hecho

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
 // Devuelve la fecha actual en formato dd/mm/yyyy
 function obtenerFechaActual(){
-   
-  
+    var fecha = new Date().toLocaleDateString();
+    return fecha;
 }
 
 // Calcula el área de un círculo a partir de su radio con dos decimales
 function calcularAreaCirculo(radio) {
- 
+    
 }
 
 // Cuenta el número de vocales en una cadena de texto
 function contarVocales(cadena) {
- 
+
 }
   
   
 // Convierte una temperatura de grados Celsius a Fahrenheit
 celsiusAFahrenheit = (celsius) => {
-  
+
 }
 
 // Crea una función que formatee un número agregando comas como separadores de miles.
 function formatearNumero(numero) {
-  
+
 }
 
 
 // Función que recibe un string en formato dd/mm/yyyy y devuelve el número de días que faltan para el 6 de Enero del año siguiente
 function reyesMagos(fecha) {
- 
-  }
+
+}
 
 
 // Ejercicio 10: Convertir a lowerCamelCase
 function camelCase(cadena) {
-  
+
 }
 
 
@@ -54,11 +54,9 @@ function calcularAleatorio(comienzo, fin){
 //   función para verificar si una cadena es un palíndromo sin utilizar expresiones regulares ni programación funcional
 function esPalindromo(cadena){
 
-  
 }
 
 // Función que recibe un número y devuelve un string con el número escrito en palabras
 function numberToWords(num) {
- 
 
 }
